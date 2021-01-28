9079005bd7457c322638fe1185609497e641c6e3
Asis Monteiro <asis_torre@hotmail.com>
2021-01-28 23:07:33 +0100

    Creacion del proyecto padre y de sus 4 módulos hijos

---
+++ practicafinal-multi-module-parent/practicafinal-multi-module-child-springrest/src/main/java/org/practicafinal/multi/module/child/springrest/App.java
@@ -0,0 +1,13 @@
+package org.practicafinal.multi.module.child.springrest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
